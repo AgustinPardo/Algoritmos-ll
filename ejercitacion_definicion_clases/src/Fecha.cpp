2bbc324469504a6e66c6045acf10ef163af2c0c5
AgustinPardo <agu_pardo@hotmail.com>
2019-09-18 19:38:16 -0300

    comienzo ej definicion de clases

--- ejercitacion_definicion_clases/src/Fecha.cpp
+++ ejercitacion_definicion_clases/src/Fecha.cpp
@@ -2,6 +2,8 @@
 // PRELUDIO //
 //////////////
 
+#include <map>
+
 // Typedef
 typedef int Anio;
 typedef int Mes;
@@ -21,11 +23,41 @@ const Mes OCTUBRE = 10;
 const Mes NOVIEMBRE = 11;
 const Mes DICIEMBRE = 12;
 
+
+std::map<Mes, int> diasMes = {{ENERO, 31}, {FEBRERO, 28},
+                         {MARZO, 31}, {ABRIL, 30},
+                         {MAYO, 31}, {JUNIO, 30},
+                        {JULIO, 31}, {AGOSTO, 31},
+                        {SEPTIEMBRE, 30}, {OCTUBRE, 31},
+                        {NOVIEMBRE, 30}, {DICIEMBRE, 31}};
+
+
 // Ejercicio 1: esBisiesto
 bool esBisiesto(Anio anio) {
+    bool out = false;
+    if(anio % 4==0 ){
+        out = true;
+    }
+    if(anio % 100==0){
+        out=false;
+    }
+    if (anio % 400==0){
+        out = true;
+    }
+    return out;
 }
 
 // Ejercicio 2: diasEnMes
+int diasEnMes(Anio anio, Mes mes){
+    int out;
+    if(esBisiesto(anio) & mes==2){
+        out=29;
+    }
+    else{
+        out=diasMes[mes];
+    }
+    return out;
+}
 //
 //
 
@@ -58,23 +90,46 @@ class Fecha {
 };
 
 // Ejercicio 3: Constructor y métodos de Fecha
-Fecha::Fecha(Anio anio, Mes mes, Dia dia) {
+Fecha::Fecha(Anio anio, Mes mes, Dia dia): _anio(anio), _mes(mes),_dia(dia) {
 }
 
 Anio Fecha::anio() const {
+    return _anio;
 }
 
 // Fecha::mes()
+Mes Fecha::mes() const {
+    return _mes;
+}
 
 // dia
-
+Dia Fecha::dia() const {
+    return _dia;
+}
 
 // Ejercicio 4: comparadores
 bool Fecha::operator==(Fecha o) const {
+    bool out=false;
+    if( _anio == o.anio() & _mes == o.mes() & _dia ==o.dia()){
+        out=true;
+    }
+    return out;
 }
 
 // Fecha::operator<
-
+bool Fecha::operator<(Fecha o) const {
+    bool out=false;
+    if (_anio < o.anio()) {
+        out = true;
+    }    
+    if( _anio == o.anio() & _mes < o.mes()) {
+        out = true;
+    }
+    if (_anio == o.anio() & _mes == o.mes() &_dia < o.dia()){
+        out=true;
+    }        
+    return out;
+}
 
 // Ejercicio 5: comparador distinto
 bool operator!=(Fecha f1, Fecha f2) {
